5a87eb6b37694710e2fa69fb7768a9c9da66fb49
gutiluis <gutierrez.luis@icloud.com>
2025-08-13 15:56:24 -0700

    view db books

--- app.py
+++ app.py
@@ -136,7 +136,10 @@ def app():
             time.sleep(1.5)
         elif choice == "2":
             # view books
-            pass    
+            for book in session.query(Book):
+                print(f"{book.id} | {book.title} | {book.author} | {book.published_date} | {book.price}")
+            input("\nPress enter to return to the main menu.")
+
         elif choice == "3":
             # search books
             pass
