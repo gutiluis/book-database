eaa90e7ecc73bd3a60b21d1edb1017d098c37cd5
gutiluis <gutierrez.luis@icloud.com>
2025-08-13 15:37:34 -0700

    add book to db function on

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from models import (Base, session,
                     Book, engine)
 import datetime
 import csv
-
+import time
 
 def menu():
     while True:
@@ -30,6 +30,34 @@ def clean_date(date_str): # date_str is any parameter variable. self-defined
               "September", "October", "November",
               "December"]
     split_date = date_str.split(" ")
+    
+    try:
+        month = int(months.index(split_date[0]) + 1) # plus 1 because months are 1-indexed
+        day = int(split_date[1].split(",")[0])
+        year = int(split_date[2])
+        return_date = datetime.date(year, month, day)
+        return return_date
+
+#    except ValueError as identifier:
+ #       print("Day is out of range for the month")
+#    except expression as identifier:
+#        print("Date is not in the correct format")
+#        pass
+    except ValueError:
+        input('''
+              \n****** DATE ERROR ******
+              \rThe date format should include a valid Month Day, Year from the past.
+              \rEx: January 13, 2003
+              \rPress enter to try again.
+              \r*************************''')
+        return
+#        pass
+    else:
+        return return_date
+#        pass
+    
+   # return
+    
     #print(split_date)
     # find the index 0 from the months list. which gives a number one less than needed. because lists are zero index
     month = int(months.index(split_date[0]) + 1) # plus 1 because months are 1-indexed
@@ -46,10 +74,20 @@ def clean_date(date_str): # date_str is any parameter variable. self-defined
 
 
 def clean_price(price_str):
+    try:
+        price_float = float(price_str)
+    except ValueError:
+        input('''
+              \n****** PRICE ERROR ******
+              \rThe price should be a number without a currency symbol.
+              \rEx: 10.99
+              \rPress enter to try again
+              \r*************************''')
+    else:
     # convert the number to a float because it has a decimal point in the .csv file
-    price_float = float(price_str)
+
     #print(price_float)
-    return int(price_float * 100)  # convert to cents by multiplying by 100
+        return int(price_float * 100)  # convert to cents by multiplying by 100
 
 
 def add_csv():
@@ -61,7 +99,6 @@ def add_csv():
             # conditional statement to check if book is still in the db 
             if book_in_db == None: # must be aware the db has dubplicates
                 
-                
             #print(row)
                 title = row[0]
                 author = row[1]
@@ -69,7 +106,7 @@ def add_csv():
                 price = clean_price(row[3])
                 new_book = Book(title=title, author=author, published_date=date, price=price)
                 session.add(new_book)
-            session.commit()
+        session.commit()
 
 
 def app():
@@ -77,12 +114,34 @@ def app():
     while app_running:
         choice = menu()
         if choice == "1":
-            pass
+            # add book
+            title = input("Title: ")
+            author = input("Author: ")
+            date_error = True # while date error
+            while date_error:
+                date = input('Published Date (Ex: October 25, 2017): ') # date in specific format for the date library/module. all integers
+                date = clean_date(date)
+                if type(date) == datetime.date:
+                    date_error = False
+            price_error = True
+            while price_error:
+                price = input("Price: (Ex: 25.64): ") # price in cents
+                price = clean_price(price)
+                if type(price) == int:
+                    price_error = False
+            new_book = Book(title=title, author=author, published_date=date, price=price)
+            session.add(new_book)
+            session.commit()
+            print("Book was added to the database.")
+            time.sleep(1.5)
         elif choice == "2":
+            # view books
             pass    
         elif choice == "3":
+            # search books
             pass
         elif choice == "4":
+            # analysys
             pass
         else:
             print("Goodbye!")
@@ -91,8 +150,9 @@ def app():
 
 if __name__ == "__main__":
     Base.metadata.create_all(engine)
-    #app()
     add_csv()
+    app()
+    #add_csv()
     #clean_date("25.64")
     #clean_price("28.84")
     for book in session.query(Book):
